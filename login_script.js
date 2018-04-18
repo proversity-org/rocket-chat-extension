7eea345e0cf1c3812fa83d513026ebed28339bb8
andrey-canon <andrey.canon@edunext.co>
2018-04-18 10:51:42 -0500

    Remove function onload

--- login_script.js
+++ login_script.js
@@ -1,18 +1,19 @@
-window.onload = function () {
+/**This scripts allows to use the authToken and userId to
+initialize the session in rocketChat.
+To set this feature, copy this content in Option-Administration-Layout-Custom-scripts
+*/
+"use strict";
 
-    "use strict";
+var urlString, url, authToken, userId;
 
-    var urlString, url, authToken, userId;
+urlString = window.location.href;
+url = new URL(urlString);
+authToken = url.searchParams.get("authToken");
+userId = url.searchParams.get("userId");
 
-    urlString = window.location.href;
-    url = new URL(urlString);
-    authToken = url.searchParams.get("authToken");
-    userId = url.searchParams.get("userId");
-
-    if (authToken !== null && userId !== null) {
-        localStorage.setItem("Meteor.loginToken", authToken);
-        localStorage.setItem("Meteor.userId", userId);
-    } else {
-        console.warn("Some parameters are missing. Please provide them");
-    }
-};
+if (authToken !== null && userId !== null) {
+    localStorage.setItem("Meteor.loginToken", authToken);
+    localStorage.setItem("Meteor.userId", userId);
+} else {
+    console.warn("Some parameters are missing. Please provide them");
+}
